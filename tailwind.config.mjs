91f844bf1e810b070449bf796165fbaaf9ae4aa7
nicodev <ndevaux@gmail.com>
2024-01-24 17:15:40 +0100

    .

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+			'./src/components/**/*.{astro,js}',
+			'./src/layouts/**/*.{astro,js}',
+			'./src/pages/**/*.{astro,js}'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
