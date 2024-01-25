6d9d33d22e142d122377f968a5e00f8c76e24bcc
nicodev <ndevaux@gmail.com>
2024-01-25 10:53:32 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import metaTags from "astro-meta-tags";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), metaTags(), robotsTxt()]
 });
